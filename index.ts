1431911080029e0698b9460495e830fd09b8bbf5
brandon-schabel <brandonschabel1995@gmail.com>
2023-04-09 18:41:59 -0700

    initial fetch test to open-ai completions api

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+const OPENAI_API_KEY = Bun.env.OPENAI_API_KEY;
+
+
+async function chatCompletion(): Promise<void> {
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: "Hello world" }],
+      }),
+    });
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+chatCompletion();
